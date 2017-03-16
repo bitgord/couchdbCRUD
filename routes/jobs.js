fe0ead4eb249e6748ec41f3ff45e9fdb6fd8ee65
Michael Gord <michael.l.gord@gmail.com>
2017-03-16 10:52:43 -0400

    init

---
+++ routes/jobs.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/add', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/show/:id', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/edit/:id', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/category/:category', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post('/add', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post('/edit/:id', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post('/delete/:id', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
+
+
+
+
+
+
+
+
